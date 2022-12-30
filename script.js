f4137ff10ebe66a0bfac5e165821848622475f3a
djsereno <djsereno91@gmail.com>
2022-12-30 00:10:00 -0800

    Add playRound function

--- script.js
+++ script.js
@@ -2,10 +2,46 @@ function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3);
   switch (choice) {
     case 0:
-      return 'Rock';
+      return 'rock';
     case 1:
-      return 'Paper';
+      return 'paper';
     case 2:
-      return 'Scissors';
+      return 'scissors';
   }
 }
+
+function playRound(playerSelection, computerSelection) {
+  
+  playerSelection = playerSelection.toLowerCase();
+
+  if (playerSelection == computerSelection) {
+    return 'Tie!';
+  }
+
+  switch (playerSelection) {
+    case 'rock':
+      if (computerSelection == 'paper') {
+        return 'You Lose! Paper beats Rock';
+      } else {
+        return 'You Win! Rock beats Scissors';
+      }
+    case 'paper':
+      if (computerSelection == 'scissors') {
+        return 'You Lose! Scissors beats Paper';
+      } else {
+        return 'You Win! Paper beats Rock';
+      }
+    case 'scissors':
+      if (computerSelection == 'rock') {
+        return 'You Lose! Rock beats Scissors';
+      } else {
+        return 'You Win! Scissors beats Paper';
+      }
+  }
+}
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+console.log('Player: ' + playerSelection);
+console.log('Computer: ' + computerSelection);
+console.log(playRound(playerSelection, computerSelection));
