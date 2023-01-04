644475921889e0091c0a4726e57f44f42e9d9cc8
djsereno <djsereno91@gmail.com>
2023-01-04 00:51:11 -0800

    Add UI mechanics and re-implement scorekeeping

--- script.js
+++ script.js
@@ -1,12 +1,72 @@
-function getComputerChoice() {
-  let choice = Math.floor(Math.random() * 3);
-  switch (choice) {
-    case 0:
-      return "rock";
-    case 1:
-      return "paper";
-    case 2:
-      return "scissors";
+const choiceButtons = document.querySelectorAll("button.choice");
+const selectionsText = document.querySelector("p.selections");
+const roundResultsText = document.querySelector("p.round-results");
+const scoresText = document.querySelector("p.scores");
+const gameResultsText = document.querySelector("p.game-results");
+const resultsContainer = document.querySelector("div.results-container");
+const newGameButton = document.querySelector("button.new-game");
+
+const playerScoreText = document.querySelector("span.player-score");
+const computerScoreText = document.querySelector("span.computer-score");
+const playerChoiceText = document.querySelector("span.player-choice");
+const computerChoiceText = document.querySelector("span.computer-choice");
+
+let playerScore;
+let computerScore;
+let roundNumber;
+let gameActive;
+
+initGame();
+choiceButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    playGame(this.innerText.toLowerCase());
+  });
+});
+newGameButton.addEventListener("click", initGame);
+
+function initGame() {
+  playerScore = 0;
+  computerScore = 0;
+  roundNumber = 0;
+  gameActive = true;
+  playerScoreText.innerText = playerScore;
+  computerScoreText.innerText = computerScore;
+  selectionsText.style.display = "none";
+  roundResultsText.style.display = "none";
+  gameResultsText.style.display = "none";
+  newGameButton.style.display = "none";
+}
+
+function playGame(playerSelection) {
+  if (gameActive) {
+    let computerSelection = getComputerChoice();
+    let results = playRound(playerSelection, computerSelection);
+
+    if (roundNumber === 0) {
+      selectionsText.style.display = "block";
+      roundResultsText.style.display = "block";
+    }
+
+    // Round only ends on non-tie. Rematch if a tie game occurs.
+    if (results.playerWin >= 0) {
+      roundNumber += 1;
+      playerScore += results.playerWin;
+      computerScore = roundNumber - playerScore;
+    }
+
+    // Update output
+    playerScoreText.innerText = playerScore;
+    computerScoreText.innerText = computerScore;
+    playerChoiceText.innerText = playerSelection.toUpperCase();
+    computerChoiceText.innerText = computerSelection.toUpperCase();
+    roundResultsText.innerText = results.message;
+
+    // End game as necessary
+    if (playerScore > 2) {
+      endGame(true);
+    } else if (computerScore > 2) {
+      endGame(false);
+    }
   }
 }
 
@@ -55,20 +115,27 @@ function playRound(playerSelection, computerSelection) {
   };
 }
 
-const buttons = document.querySelectorAll("button");
-buttons.forEach((button) => {
-  button.addEventListener("click", function (e) {
-    let playerSelection = this.innerText.toLowerCase();
-    let computerSelection = getComputerChoice();
-    let results = playRound(playerSelection, computerSelection);
+function getComputerChoice() {
+  let choice = Math.floor(Math.random() * 3);
+  switch (choice) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
+  }
+}
 
-    console.log("--------------------------------------------");
-    console.log(
-      "Player: " +
-        playerSelection.toUpperCase() +
-        ", Computer: " +
-        computerSelection.toUpperCase()
-    );
-    console.log(results.message);
-  });
-});
+function endGame(playerWin) {
+  gameActive = false;
+  if (playerWin) {
+    gameResultsText.innerText = "GAME OVER: You win! Great job!";
+    newGameButton.innerText = "Play Again?";
+  } else {
+    gameResultsText.innerText = "GAME OVER: You lose! Better luck next time!";
+    newGameButton.innerText = "Try Again?";
+  }
+  gameResultsText.style.display = "block";
+  newGameButton.style.display = "block";
+}
